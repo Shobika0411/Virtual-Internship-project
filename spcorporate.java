1ced78929369daa8e26b2b28c6492f6d5fdba251
Shobika0411 <146432671+Shobika0411@users.noreply.github.com>
2024-08-24 11:21:05 +0530

    Create spcorporate.java

---
+++ spcorporate.java
@@ -0,0 +1,67 @@
+package praveen;
+
+public class workercorporate {
+	int workerid;
+	String name;
+	String gender;
+	String mobile;
+	String year;
+	String city;
+	String com;
+	String email;
+	String course;
+	public int getWorkerid() {
+		return workerid;
+	}
+	public void setWorkerid(int workerid) {
+		this.workerid = workerid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCom() {
+		return com;
+	}
+	public void setCom(String com) {
+		this.com = com;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+}
